f2c9afd50453c958be787ed7380bca0b240cd2b6
Ashutosh Kumar <89918888+Invacui@users.noreply.github.com>
2022-09-24 18:30:23 +0530

    Update FizzBuzz_Hacker-Rank.cpp

--- FizzBuzz_Hacker-Rank.cpp
+++ FizzBuzz_Hacker-Rank.cpp
@@ -23,3 +23,24 @@ int main() {
 		}
 		
 	}
+	
+	void fizzBuzz(int n) {
+for(int i = 1; i<=n ; i++)
+{
+    if(i%3==0 && i%5==0)
+    {
+        std::cout<<"FizzBuzz"<<"\n";
+    }
+    else if(i%3==0 && i%5 != 0)
+    {
+        std::cout<<"Fizz"<<"\n";
+    }
+    else if(i%3 != 0 && i%5 == 0)
+    {
+        std::cout<<"Buzz"<<"\n";
+    }
+    else {
+    std::cout<<i<<"\n";
+    }
+}
+}
